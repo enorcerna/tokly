3958db93a18b84a403e1706a0c8314a73a7412bd
Enor Cerna <u17200479@utp.edu.pe>
2024-11-20 21:50:12 -0500

    paste button ui

---
+++ src/components/page/Paste.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Icon from '@iconify/svelte';
+	import { Button } from '../ui/button';
+	import { pasteStore } from '@/stores/download';
+	const handlePaste = () => {
+		navigator.clipboard.readText().then((value) => {
+			if (value.includes('tiktok.com')) {
+				pasteStore.set(value);
+			}
+		});
+	};
+</script>
+
+<Button variant="secondary" onclick={handlePaste} size="icon">
+	<Icon icon="solar:copy-line-duotone" />
+</Button>
