aed860f1199cb1414e70f3e19b85edc36b849ac0
Enor Cerna <u17200479@utp.edu.pe>
2024-11-21 15:21:20 -0500

    entry up server

--- server.ts
+++ server.ts
@@ -1,5 +1,21 @@
+/// file: my-server.js
+import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
 import { handler } from './build/handler.js';
-import express from 'express';
-const app = express();
+
+const app = new Application();
+
+// add a route that lives separately from the SvelteKit app
+const router = new Router();
+router.get('/healthcheck', (ctx) => {
+	ctx.response.body = 'ok';
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
-app.listen(8000);
+
+app.addEventListener('listen', () => {
+	console.log('listening on port 8000');
+});
+await app.listen({ port: 8000 });
