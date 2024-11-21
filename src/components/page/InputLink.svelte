fa95e0e8438f4d70a432f2cfa35a73d05e77295f
Enor Cerna <u17200479@utp.edu.pe>
2024-11-20 21:50:00 -0500

    input link ui

---
+++ src/components/page/InputLink.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { pasteStore } from '@/stores/download';
+	import { Input } from '../ui/input';
+	let link = $state('');
+	$effect(() => {
+		pasteStore.subscribe((v) => {
+			link = v;
+		});
+	});
+</script>
+
+<Input class="" placeholder="Pega tu enlace ..." bind:value={link} />
