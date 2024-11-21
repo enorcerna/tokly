c0cd51b05b463de02e8bf6d80a1d7fc76334beff
Enor Cerna <u17200479@utp.edu.pe>
2024-11-20 21:48:16 -0500

    responsive ui

---
+++ src/components/global/ResponsiveContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface Props extends HTMLAttributes<HTMLElement> {}
+	import { cn } from '@/utils';
+
+	let { class: className, children, ...rest }: Props = $props();
+</script>
+
+<div class={cn(' mx-auto  lg:w-3/4', className)} {...rest}>
+	{@render children?.()}
+</div>
