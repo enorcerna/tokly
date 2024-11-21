3c2b49443a7ed78057d956337a7a69c5446e53b1
Enor Cerna <u17200479@utp.edu.pe>
2024-11-20 21:49:53 -0500

    get button ui

---
+++ src/components/page/GetVideo.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { pasteStore } from '@/stores/download';
+	import { Button } from '../ui/button';
+	import Icon from '@iconify/svelte';
+	const handleGetVideo = () => {
+		if ($pasteStore.includes('tiktok.com')) {
+			goto(`?url=${$pasteStore}`);
+		}
+	};
+</script>
+
+<Button onclick={handleGetVideo} size="lg">
+	<Icon icon="solar:clapperboard-play-line-duotone" />
+	Obtener
+</Button>
